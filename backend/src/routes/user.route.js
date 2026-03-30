52be79195b59ddf6de4b78b38a9479785393b015
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-03-31 00:49:05 +0530

    add file upload

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createUser, getUsers, getUserById } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", createUser);
+router.post("/create", upload.single("avatar"), createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 
